82c5ac2a23f658d9f9a1d84e924d141f828e6962
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-12-10 14:55:35 +0530

    Create Find Nums.py

---
+++ MATH /Find Nums.py
@@ -0,0 +1,19 @@
+-->https://my.newtonschool.co/playground/code/8uddo24k1kgc
+
+
+
+def find_nums(l, r):
+    def gcd(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
+    for a in range(2, r):
+        b = r - a
+        if l <= a + b <= r and gcd(a, b) != 1:
+            return "Yes"
+    
+    return "No"
+
+l,r=map(int,input().split())
+print(find_nums(l,r))
