e970fe4c66dc04b3568b9b5332e38efeabdf8797
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-12-28 12:34:24 +0530

    Create Distribute Chocolates.py

---
+++ ARRAY/Distribute Chocolates.py
@@ -0,0 +1,12 @@
+-->https://my.newtonschool.co/playground/code/enes108tkzkh
+
+
+
+# Your code here
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+s=sum(l)
+if s >= n :
+    print("Yes")
+else :
+    print("No")
