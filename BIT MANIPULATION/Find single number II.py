1bcec8938fa9ecd29d22ea4e3d53b775c96f616c
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-01-10 12:37:57 +0530

    Create Find single number II.py

---
+++ BIT MANIPULATION/Find single number II.py
@@ -0,0 +1,15 @@
+--->https://my.newtonschool.co/playground/code/s4wyrcd7n4o8
+
+
+
+# Your code here
+n=int(input())
+l=list(map(int,input().split()))
+s=0
+for i in range(32) :
+    f=0
+    for j,v in enumerate(l) :
+        if v>>i & 1==1:
+            f +=1
+    s += (f%3)<<i 
+print(s)
