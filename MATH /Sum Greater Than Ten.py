57dda2ae7c0b7f2018386beb44c1b73873125596
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-01-20 19:27:53 +0530

    Create Sum Greater Than Ten.py

---
+++ MATH /Sum Greater Than Ten.py
@@ -0,0 +1,10 @@
+--->https://my.newtonschool.co/playground/code/xjso8bu9gorw
+
+
+
+# Your code here
+a,b,c=map(int,input().split())
+if a+b >= 10 or b+c>=10 or a+c >=10 :
+    print("YES")
+else :
+    print("NO")
