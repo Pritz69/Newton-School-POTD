129ada0580f8764a62918c4efbd2f5ed56561231
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-01-31 22:00:01 +0530

    Update Reach the end.py

--- DP/Reach the end.py
+++ DP/Reach the end.py
@@ -1,6 +1,15 @@
 --->https://my.newtonschool.co/playground/code/vvzvbsg0oeio
 
 
+n = int(input())
+mod = 10**9 + 7
+dp=[0]*(n+1)
+dp[0]=1
+dp[1]=1
+for i in range(2,n+1) :
+    dp[i]=(dp[i-1]+dp[i-2])%mod 
+print(dp[n])
+
 
 import sys  
 sys.setrecursionlimit(1000000) 
