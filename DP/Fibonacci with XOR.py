46fd39c2dcf1a9187e904f7375ce9214bd4c0b3b
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-02-05 11:11:42 +0530

    Create Fibonacci with XOR.py

---
+++ DP/Fibonacci with XOR.py
@@ -0,0 +1,12 @@
+---->https://my.newtonschool.co/playground/code/4ardoqec7fc0
+
+
+
+# Your code here
+a,b,n=map(int,input().split())
+if (n+1)%3==0 :
+    print(a^b)
+elif (n+1)%3==2 :
+    print(b)
+else :
+    print(a)
