b612409596cbdcb628311f8ba8e469a0f146aa54
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-02-06 18:10:09 +0530

    Create Exchange Wallet.py

---
+++ MATH /Exchange Wallet.py
@@ -0,0 +1,14 @@
+--->https://my.newtonschool.co/playground/code/4jwv6nnfovsb
+
+
+
+# Your code here
+a,b=map(int,input().split())
+if b%2==1 and a%2==1 :
+    print("Raj")
+elif b%2==0 and a%2==1:
+    print("Tia")
+elif b%2==1 and a%2==0:
+    print("Tia")
+else :
+    print("Raj")
