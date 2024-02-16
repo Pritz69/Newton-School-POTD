9a0d46fbffbc9556fd2a55f19ed0ed41cf63afca
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-02-16 20:40:27 +0530

    Create Equal AB.py

---
+++ BIT MANIPULATION/Equal AB.py
@@ -0,0 +1,12 @@
+---->https://my.newtonschool.co/playground/code/3dwt26pvrxmh
+
+
+
+# Your code here
+a,b=int(input()),int(input())
+if a==b :
+    print(0)
+elif (a&b)==a or (a&b)==b :
+    print(1)
+else :
+    print(2)
