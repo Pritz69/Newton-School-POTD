7efa2d5b23e347cea631a82b7388a3c6b2167160
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-02-23 11:47:36 +0530

    Create A or B?.py

---
+++ String/A or B?.py
@@ -0,0 +1,12 @@
+---->https://my.newtonschool.co/playground/code/n14pafapulnv
+
+
+
+# Your code here
+c=0
+for x in input() :
+    if x=='A' :
+        c +=1
+    else :
+        c -=1
+print(abs(c))
