8f66942168859b340dfbfeb98e772fff27125fc3
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-04-04 22:31:32 +0530

    Create Divisible Number Sum.py

---
+++ MATH /Divisible Number Sum.py
@@ -0,0 +1,11 @@
+---->https://my.newtonschool.co/playground/code/khnutwa6mzux
+
+
+
+# Your code here
+s=0
+n=int(input())
+for i in range(1,n+1) :
+    if i%2==0 or i%3==0 or i%7==0 :
+        s += i 
+print(s)
