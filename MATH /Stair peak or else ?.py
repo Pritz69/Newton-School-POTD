379a34f4991b54603f2eb6fcac11a18477828beb
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-04-19 11:37:15 +0530

    Create Stair peak or else ?.py

---
+++ MATH /Stair peak or else ?.py
@@ -0,0 +1,13 @@
+---->https://my.newtonschool.co/playground/code/f6qk5bwu1dsx
+
+
+
+# Your code here
+a,b,c=map(int,input().split())
+if a< b and b<c :
+    print("Stair")
+elif b>a and b>c :
+    print("Peak")
+
+else :
+    print("Nothing")
