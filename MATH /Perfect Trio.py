43b2c033470c272c418a0be34f2e26c2ad46fa3b
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-06 09:40:18 +0530

    Create Perfect Trio.py

---
+++ MATH /Perfect Trio.py
@@ -0,0 +1,11 @@
+# Your code here
+for _ in range(int(input())) :
+    a,b,c=map(int,input().split())
+    if a==(b+c) :
+        print("Yes")
+    elif b==(a+c) :
+        print("Yes")
+    elif c==(a+b) :
+        print("Yes")
+    else :
+        print("No")
