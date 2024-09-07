8977a149b71983a82b15ebeafa8db370d9ef5622
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-09-07 14:50:39 +0530

    Create The Minimum XOR.py

---
+++ BIT MANIPULATION/The Minimum XOR.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for i in range(int(input())):
+    n=int(input())
+    l=list(map(int,input().split()))
+    x=0
+    for e in l :
+        x=x^e 
+    ans=x
+    for e in l :
+        ans=min(ans,x^e)
+    print(ans)
